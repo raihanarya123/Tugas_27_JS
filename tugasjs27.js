46825629c393ee68169708ac720c842ead66609d
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:18:35 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs27.js
@@ -0,0 +1,13 @@
+function tugas27() {
+    var data1 = "No";
+    var data2 = "rumah saya";
+    var data3 = 3;
+    var data4 = "A";
+    var final = data1+" "+data2+" "+data3 + data4;
+    console.log(final);
+    var kata = "Jalan pangeran Tirtayasa No"
+    let number = 10;
+    kata = kata.concat(number);
+    console.log(kata);
+}
+tugas27();
